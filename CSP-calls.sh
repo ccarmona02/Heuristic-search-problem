df1120a3c01961ea3b104a0934c84e7fab4ea3c9
Carlos Carmona Ramos <82317112+ccarmona02@users.noreply.github.com>
2022-12-13 11:11:43 +0100

    bash for running all testing commands
    Lasts too long, we have to come up with cases that do not have such large numbers of solutions.

---
+++ CSP-calls.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+#run several tests 
+python3 CSPBusSeats.py ./CSP-tests/students1
+python3 CSPBusSeats.py ./CSP-tests/students2
+python3 CSPBusSeats.py ./CSP-tests/students3
+python3 CSPBusSeats.py ./CSP-tests/students4
